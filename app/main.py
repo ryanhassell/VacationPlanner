a4508127e852f9c27af0d13f3e3282454c53af5f
ryanhassell <ryanhassell5656@gmail.com>
2025-02-02 22:54:55 -0500

    change db name

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from routers import users
+from routers import users, groups
 
 app = FastAPI()
 origins = [
@@ -8,4 +8,4 @@ origins = [
 ]
 
 app.include_router(users.router, prefix="/users", tags=["Users"])
-app.include_router(users.router, prefix="/groups", tags=["Groups"])
+app.include_router(groups.router, prefix="/groups", tags=["Groups"])
