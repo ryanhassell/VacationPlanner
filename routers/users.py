2388778538d1c083e53418dbafc8a72d765ef996
ryanhassell <ryanhassell5656@gmail.com>
2025-02-02 22:10:35 -0500

    added create user

--- routers/users.py
+++ routers/users.py
@@ -4,7 +4,8 @@ from sqlalchemy.orm import sessionmaker
 from app.global_vars import DB_HOST, DB_NAME, DB_PASSWORD, DB_USERNAME
 from fastapi import FastAPI, HTTPException, Depends, APIRouter
 from app.models import Base, User
-from schemas.user import UserResponse
+from schemas.user import UserResponse, UserCreate
+
 # Define your connection string
 conn_string = f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
 engine = create_engine(conn_string)
@@ -27,7 +28,7 @@ def get_db():
         db.close()
 
 
-@router.get("/{uid}", response_model=list[UserResponse])
+@router.get("/{uid}", response_model=UserResponse)
 async def get_user_by_uid(uid: int, db: Session = Depends(get_db)):
     users = db.query(User).filter(User.uid == uid)
     return users
@@ -43,3 +44,21 @@ async def list_users(skip: int = 0, limit: int = 10, db: Session = Depends(get_d
 async def list_users_by_gid(gid: int, db: Session = Depends(get_db)):
     users = db.query(User).filter(User.gid == gid).all()
     return users
+
+
+@router.post("", response_model=UserResponse)
+async def create_user(user: UserCreate, db: Session = Depends(get_db)):
+    # Create a new user in the database
+    new_user = User(
+        uid=user.uid,
+        first_name=user.first_name,
+        last_name=user.last_name,
+        email_address=user.email_address,
+        phone_number=user.phone_number,
+        password=user.password,
+        groups=user.groups
+    )
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
