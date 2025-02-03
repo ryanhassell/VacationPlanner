9ba959ae9875f7aedafb694e13891015086d7395
JackGallagher41 <143018901+JackGallagher41@users.noreply.github.com>
2025-02-02 21:01:23 -0500

    Set the Global Vars

--- app/global_vars.py
+++ app/global_vars.py
@@ -1,10 +1,4 @@
-from fastapi import FastAPI
-#from routers import
-
-app = FastAPI()
-origins = [
-    "http://192.168.1.172/",
-    "http://192.168.1.172/:8000",
-]
-
-#app.include_router()
+USERNAME="postgres"
+PASSWORD="123456789"
+HOST="localhost"
+NAME="HomeworkTracker"
