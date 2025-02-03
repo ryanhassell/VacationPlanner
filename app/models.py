966d2bf6b1c881a9791b39c8fe2d01040de52d3f
JackGallagher41 <143018901+JackGallagher41@users.noreply.github.com>
2025-02-02 21:25:15 -0500

    Set the Global Vars

--- app/models.py
+++ app/models.py
@@ -17,9 +17,17 @@ Base = declarative_base()
 
 class User(Base):
     __tablename__ = "users"
-    id = Column(Integer, primary_key=True, index=True)
+    uid = Column(Integer, primary_key=True, index=True)
     first_name = Column(String)
     last_name = Column(String)
     email_address = Column(String)
     phone_number = Column(String)
     password = Column(String)
+    groups= Column(ARRAY(Integer))
+
+class Group(Base):
+    __tablename__ = "groups"
+    gid = Column(Integer, primary_key=True, index=True)
+    members = Column(ARRAY(Integer))
+    owner = Column(Integer)
+    admin = Column(ARRAY(Integer))
--- schemas/user.py
+++ schemas/user.py
@@ -2,13 +2,14 @@ from pydantic import BaseModel
 
 
 class UserResponse(BaseModel):
-    id: int
+    uid: int
     first_name: str
     last_name: str
     email_address: str
     phone_number: str
     #user_type: Enum
     password: str
+    groups: int
 
     class Config:
         arbitrary_types_allowed = True
