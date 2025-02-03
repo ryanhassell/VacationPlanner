c5e90a6381776194d1586f8f1451900963c7c2a4
ryanhassell <ryanhassell5656@gmail.com>
2025-02-02 22:41:51 -0500

    change db name

--- app/main.py
+++ app/main.py
@@ -3,8 +3,8 @@ from routers import users
 
 app = FastAPI()
 origins = [
-    "http://192.168.1.172/",
-    "http://192.168.1.172/:8000",
+    "http://localhost/",
+    "http://localhost/:8000",
 ]
 
 app.include_router(users.router, prefix="/users", tags=["Users"])
