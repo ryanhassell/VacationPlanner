c32b53c05f0d65e19a081fac43384c674a2e376a
JackGallagher41 <143018901+JackGallagher41@users.noreply.github.com>
2025-02-16 23:21:18 -0500

    View groups base page update

--- lib/view_groups_page.dart
+++ lib/view_groups_page.dart
@@ -5,12 +5,27 @@ class ViewGroupsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Dummy list of groups
+    final List<String> groups = ['Group 1', 'Group 2', 'Group 3'];
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('View Groups'),
       ),
-      body: Center(
-        child: const Text('List of your groups will be shown here!'),
+      body: ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemCount: groups.length,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8.0),
+            child: ElevatedButton(
+              onPressed: () {
+                // Handle navigation to group details
+              },
+              child: Text(groups[index]),
+            ),
+          );
+        },
       ),
     );
   }
