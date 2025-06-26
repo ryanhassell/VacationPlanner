7ba295ab88eb3f7bb5285d222dcc2e9d0254e03f
ryanhassell <143541064+ryanhassell@users.noreply.github.com>
2025-06-26 16:30:14 -0400

    get rid of key

--- lib/global_vars.dart
+++ lib/global_vars.dart
@@ -2,4 +2,4 @@ const String apiKey = '';
 const String ip = "10.0.0.227:8000";
 String userType = '';
 int current_user_id = 0;
-const String mapboxAccessToken = "sk.eyJ1IjoiY2hlZXNlZnJpZXMiLCJhIjoiY204bmJjc2toMDBnMjJ5cHpkaWQ0aWVldSJ9.j3l4E43PL3P1MKT_KtjUTw";
+const String mapboxAccessToken = "key";
